c9d103c6746b46da4383a31c9817b1f5688ff59f
Jelle van der Waa <jelle@vdwaa.nl>
2012-01-12 20:54:51 +0100

    use a method for parsing the bugtracker page
    Signed-off-by: Jelle van der Waa <jelle@vdwaa.nl>

--- bin/unassignedbugs.py
+++ bin/unassignedbugs.py
@@ -5,6 +5,8 @@ from BeautifulSoup import BeautifulSoup
 import smtplib
 import ConfigParser
 
+
+# Retreive user information
 config = ConfigParser.ConfigParser()
 config.read('config.cfg')
 user = config.get('data','user')
@@ -18,14 +20,11 @@ login_page='https://bugs.archlinux.org/index.php?do=authenticate'
 msg = "To: %s \nFrom: %s \nSubject: Bug Mail\n\n" % (toaddr,fromaddr)
 msg += 'Unassigned bugs  \n\n'
 
-
 # build opener with HTTPCookieProcessor
 o = urllib2.build_opener( urllib2.HTTPCookieProcessor() )
 urllib2.install_opener( o )
 
-p = urllib.urlencode( { 'user_name': user, 'password': password, 'remember_login' : 'on',
-                #'return_to' : None,
-} )
+p = urllib.urlencode( { 'user_name': user, 'password': password, 'remember_login' : 'on',} )
 f = o.open(login_page, p)
 data = f.read()
 
@@ -36,30 +35,28 @@ url = "https://bugs.archlinux.org/index.php?string=&project=1&search_name=&type%
 url2= "https://bugs.archlinux.org/index.php?string=&project=5&search_name=&type%5B%5D=&sev%5B%5D=&pri%5B%5D=&due%5B%5D=0&reported%5B%5D=&cat%5B%5D=&status%5B%5D=1&percent%5B%5D=&opened=&dev=&closed=&duedatefrom=&duedateto=&changedfrom=&changedto=&openedfrom=&openedto=&closedfrom=&closedto=&do=index"
 
 
+def parse_bugtrackerpage(url):
+    # open bugtracker / parse 
+    page = urllib2.urlopen(url)
+    soup =  BeautifulSoup(page)
+    data = soup.findAll('td',{'class':'task_id'})
+    msg = ""
 
-# open bugtracker / parse 
-page = urllib2.urlopen(url)
-soup =  BeautifulSoup(page)
-foo = soup.findAll('td',{'class':'task_id'})
+    # print all found bugs
+    for f in data:
+        title = f.a['title'].replace('Assigned |','')
+        title = f.a['title'].replace('| 0%','')
+        msg += '* [http://bugs.archlinux.org/task/%s FS#%s] %s \n' % (f.a.string,f.a.string,title)
 
-# print all found bugs
-for f in foo:
-    title = f.a['title'].replace('Assigned |','')
-    title = f.a['title'].replace('| 0%','')
-    msg += '* [http://bugs.archlinux.org/task/%s FS#%s] %s \n' % (f.a.string,f.a.string,title)
+    return msg
 
-# open bugtracker / parse 
-page = urllib2.urlopen(url2)
-soup =  BeautifulSoup(page)
-foo = soup.findAll('td',{'class':'task_id'})
-
+msg += parse_bugtrackerpage(url)
 msg += '\n\nCommunity: \n\n'
+msg += parse_bugtrackerpage(url2)
 
-# print all found bugs
-for f in foo:
-    title = f.a['title'].replace('Assigned |','')
-    title = f.a['title'].replace('| 0%','')
-    msg += '* [http://bugs.archlinux.org/task/%s FS#%s] %s \n' % (f.a.string,f.a.string,title)
+
+
+
 
 msg = msg.encode("utf8")
 # send mail
