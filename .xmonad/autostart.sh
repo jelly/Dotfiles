d1c6d8f90cae7eb20aceb9bc1359153d1748d499
Jelle van der Waa <jelle@vdwaa.nl>
2012-04-12 21:50:07 +0200

    added missing autostart file Signed-off-by: Jelle van der Waa <jelle@vdwaa.nl>

---
+++ .xmonad/autostart.sh
@@ -0,0 +1,13 @@
+if pgrep -u jelle nm-applet &> /dev/null;
+then
+  echo alive &> /dev/null
+else
+    nm-applet&
+fi
+if pgrep -u jelle trayer &> /dev/null;
+then
+  echo alive &> /dev/null
+else
+  trayer --edge top --align left   --SetDockType true --SetPartialStrut true --width 3 --height 2 
+  /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1&
+fi
