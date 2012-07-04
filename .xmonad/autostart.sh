eeeaefc605b047ea4695b0318e83619da8d863de
Jelle van der Waa <jelle@vdwaa.nl>
2012-07-04 08:37:13 +0200

    fix trayer for bigger networkmanager icon
    Signed-off-by: Jelle van der Waa <jelle@vdwaa.nl>

--- .xmonad/autostart.sh
+++ .xmonad/autostart.sh
@@ -8,6 +8,6 @@ if pgrep -u jelle trayer &> /dev/null;
 then
   echo alive &> /dev/null
 else
-  trayer --edge top --align left   --SetDockType true --SetPartialStrut true --width 3 --height 2 
+  trayer --edge top --align left   --SetDockType true --SetPartialStrut true --width 3 --height 18
   /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1&
 fi
