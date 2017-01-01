78f9249dede00150bac7157dd2c54f97a21d2186
Jelle van der Waa <jelle@vdwaa.nl>
2017-01-01 20:59:50 +0100

    Add mail-archive, change logic to a list of urls

--- .mutt/open_ml_url.py
+++ .mutt/open_ml_url.py
@@ -7,21 +7,7 @@ from email.parser import Parser
 import requests
 
 
-def open_marc_info(message_id):
-    url = 'http://marc.info/?i={}'.format(message_id)
-    r = requests.get(url)
-    if r.status_code == 200:
-        subprocess.call(['xdg-open', url], stdout=subprocess.DEVNULL)
-    return False
-
-
-def open_gmane(message_id):
-    url = 'http://mid.gmane.org/{}'.format(message_id)
-    r = requests.get(url)
-    if b'NOT FOUND' not in r.content:
-        subprocess.call(['xdg-open', url], stdout=subprocess.DEVNULL)
-        return True
-    return False
+URLS = ['https://www.mail-archive.com/search?l=mid&q={}', 'http://marc.info/?i={}', 'http://mid.gmane.org/{}']
 
 
 if __name__ == "__main__":
@@ -30,6 +16,12 @@ if __name__ == "__main__":
         eml += line
     headers = Parser().parsestr(eml)
     if 'message-id' in headers:
-        message_id = headers['message-id']
-        if not open_marc_info(message_id):
-            open_gmane(message_id)
+        for base_url in URLS:
+            message_id = headers['message-id'].replace('<', '').replace('>', '')
+            url = base_url.format(message_id)
+            r = requests.get(url)
+            if r.status_code == 200 and b'NOT FOUND' not in r.content:
+                subprocess.call(['xdg-open', url], stdout=subprocess.DEVNULL)
+                break
+
+
