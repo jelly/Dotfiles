ecbf205f1ed8126cc291c54b0f8aa9a7732a07e2
Jelle van der Waa <jelle@vdwaa.nl>
2022-04-19 17:51:32 +0200

    drop unused stuff from statusline

--- .config/nvim/lua/jelle/statusline.lua
+++ .config/nvim/lua/jelle/statusline.lua
@@ -12,7 +12,7 @@ require('lualine').setup {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = {'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
