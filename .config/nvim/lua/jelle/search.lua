7332579f18c7c4c5613aa94d688ec16d7b667b5d
Jelle van der Waa <jelle@vdwaa.nl>
2022-08-18 18:35:38 +0200

    nvim: add yank preview selection keybinding

--- .config/nvim/lua/jelle/search.lua
+++ .config/nvim/lua/jelle/search.lua
@@ -1,3 +1,14 @@
+-- Yanks the shown preview line
+local yank_selection = function(prompt_bufnr)
+    local action_state = require "telescope.actions.state"
+    local actions = require "telescope.actions"
+    local content = action_state.get_selected_entry()
+    if content == nil then
+        return
+    end
+    vim.fn.setreg('', content["text"])
+end
+
 require('telescope').setup({
   defaults = {
     -- Default configuration for telescope goes here:
@@ -9,7 +20,8 @@ require('telescope').setup({
         -- map actions.which_key to <C-h> (default: <C-/>)
         -- actions.which_key shows the mappings for your picker,
         -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-        ["<C-h>"] = "which_key"
+        ["<C-h>"] = "which_key",
+	["<C-y>"] = yank_selection
       }
     }
   },
