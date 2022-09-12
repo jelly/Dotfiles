4f62f3a1fc6c7f04343c04e227ee66ff194fa6d3
Jelle van der Waa <jelle@vdwaa.nl>
2022-09-12 16:22:59 +0200

    Up snippet keyword length and order

--- .config/nvim/lua/jelle/cmp.lua
+++ .config/nvim/lua/jelle/cmp.lua
@@ -38,8 +38,11 @@ cmp.setup {
     end,
   },
   sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
+    { name = 'path' },
+    { name = 'nvim_lsp', keyword_length = 3 },
+    { name = 'buffer', keyword_length = 3},
+    { name = 'luasnip', keyword_length = 2 },
+
     { name = "crates" },
   },
 }
