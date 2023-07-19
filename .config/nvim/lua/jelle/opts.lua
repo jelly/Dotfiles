e16ec4251aa7cdb36322eed0bcfd2a2950593c68
Jelle van der Waa <jelle@vdwaa.nl>
2023-07-19 11:40:07 +0200

    nvim: allow reloading of the theme via SIGUSR1

--- .config/nvim/lua/jelle/opts.lua
+++ .config/nvim/lua/jelle/opts.lua
@@ -11,8 +11,6 @@ require("tokyonight").setup({
   end
 })
 
-vim.cmd[[colorscheme tokyonight]]
-
 -- Leader
 vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
 vim.g.mapleader = ' '
@@ -23,3 +21,26 @@ vim.o.termguicolors = true
 
 -- Clipboard
 vim.opt.clipboard = 'unnamedplus'
+
+-- Theme
+vim.cmd.colorscheme('tokyonight')
+
+local function update_theme()
+	vim.fn.jobstart({ 'gsettings', 'get', 'org.gnome.desktop.interface', 'color-scheme'}, {
+		stdout_buffered = true,
+		on_stdout = function(_, data)
+			if data then
+				theme = table.concat(data)
+				if theme == "'prefer-dark'" then
+					vim.opt.background = 'dark'
+				else
+					vim.opt.background = 'light'
+				end
+			end
+		end
+	})
+end
+
+update_theme()
+
+vim.api.nvim_create_autocmd("Signal", { pattern = { "*" }, callback = update_theme })
