2ddb4c6ce3e77414d4a1f9c5dd5437e8a6d6917d
Jelle van der Waa <jelle@vdwaa.nl>
2024-09-23 10:04:37 +0200

    nvim: disable language plugins I don't use

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,3 +12,8 @@ require("jelle.vimwiki")
 require("jelle.dap")
 require("jelle.cov")
 require("jelle.test")
+
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
